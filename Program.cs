e50161fd29ec9f383ef0b117366a614b4ab347eb
nikitai9 <87224198+nikitai9@users.noreply.github.com>
2023-07-22 13:01:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+namespace WinFormsApp1
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            UserModel userModel = new UserModel() { Username = "JohnDoe" };
+            UserViewForm userViewForm = new UserViewForm(new UserController(userModel, new UserViewForm()));
+
+            Application.Run(userViewForm);
+        }
+    }
+}
