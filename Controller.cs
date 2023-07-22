c90e0c7b018186b361981dcd5f11ba494404ee11
nikitai9 <87224198+nikitai9@users.noreply.github.com>
2023-07-22 13:00:01 +0300

    MVC

---
+++ Controller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    public class UserController
+    {
+        private UserModel model;
+        private UserViewForm view;
+
+        public UserController(UserModel model, UserViewForm view)
+        {
+            this.model = model;
+            this.view = view;
+            this.view.UpdateView(this.model.Username);
+        }
+
+        public void UpdateUsername(string newUsername)
+        {
+            model.Username = newUsername;
+            view.UpdateView(model.Username);
+        }
+
+        public void ShowView()
+        {
+            view.ShowDialog();
+        }
+    }
+}
---
+++ View.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    public partial class UserViewForm : Form
+    {
+        private UserController controller;
+
+        public UserViewForm(UserController controller)
+        {
+            InitializeComponent();
+            this.Controller = controller;
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserController Controller { get => controller; set => controller = value; }
+
+    }
+}
